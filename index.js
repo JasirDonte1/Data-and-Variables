b96e3591e1eb469f94affa5fcc27c734f70a9e18
Jasir Massey-Campbell <33403498+JasirDonte1@users.noreply.github.com>
2021-06-11 00:39:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// FORK this starter file and save it to your own Repl.it account.
+
+
+// Declare and initialize the 12 variables here:
+let shuttleName = 'A shuttle name';
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760_000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let fuelTempCelsius = -225;
+let fuelLevel = 100;
+let weatherStatus = 'clear';
+
+// Write code to generate the LC04 report here:
+
+console.log("-------------------------------------");
+console.log("> LC04 - LAUNCH CHECKLIST");
+console.log("-------------------------------------");
+console.log("Date: " + date);
+console.log("Time: " + time + "\n");
+console.log("-------------------------------------");
+console.log("> ASTRONAUT INFO");
+console.log("-------------------------------------");
+console.log("> FUEL DATA");
+console.log("* count: " + astronautCount);
+console.log("* status: " + astronautStatus + "\n");
+console.log("-------------------------------------");
+console.log("* Fuel temp celsius: " + fuelTempCelsius);
+console.log("* Fuel level:" + fuelLevel + "\n");
+console.log("-------------------------------------");
+console.log("> MASS DATA");
+console.log("-------------------------------------");
+console.log("* Crew mass: " + crewMassKg);
+console.log("* Fuel mass: " + fuelMassKg);
+console.log("* shuttle mass: " + shuttleMassKg);
+console.log("* Total mass: " + totalMassKg + "\n");
+console.log("-------------------------------------");
+console.log("> FLIGHT PLAN");
+console.log("-------------------------------------");
+console.log("* weather: "+ weatherStatus + "\n");
+console.log("-------------------------------------");
+console.log("> OVERALL STATUS");
+console.log("-------------------------------------");
+console.log("* Clear for takeoff: YES")
+
+// When done, have your TA check your code.
+
+
+// BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
